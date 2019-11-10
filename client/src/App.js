179bab145f5b4055b764ec8083eecf7c72d9a83a
vermad843 <vishnuverma7524@gmail.com>
2019-11-11 01:18:49 +0530

    adding svg

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import  './App.css'
-
+import svg from './logo.svg'
 
 
 
@@ -11,13 +11,20 @@ class App extends Component {
          title : 'App For Fun'
      }
     }
+
+
+  
     render() {
         const {title} = this.state;
         return (
-            <div>
-                <h1 className = 'title'>
+            <div className = "App">
+               <section className = 'title'>
+               <h1 >
                     {title} 
                 </h1>
+                <img className = "code" src = {svg}/>
+               </section>
+               
                 <form className = "form">
                     <label htmlFor = "name">Name</label>
                     <input 
